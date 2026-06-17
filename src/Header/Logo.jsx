3c3539ab0bfa3f30db3c9ff27126b5c40f375417
Alireza Sadeghi <alirezagt4x@gmail.com>
2026-06-18 01:44:05 +0330

    Update logo and add light mode logo

--- src/Header/Logo.jsx
+++ src/Header/Logo.jsx
@@ -2,7 +2,8 @@ export default function Logo(){
     return(
         <div className="flex">
             <img src="/public/Images/ListItIcon.webp" alt="ListIt icon" className="w-12" />
-            <img src="/public/Images/ListItLogo.webp" alt="ListIt logo" className="w-24" />
+            <img src="/public/Images/ListItLogo.webp" alt="ListIt logo" className="w-28 hidden dark:flex" />
+            <img src="/public/Images/ListItLogoLight.webp" alt="ListIt logo" className="w-28 flex dark:hidden" />
         </div>
     )
 }
