677afe910fc8dc9068089f39573e1ff5cec2fde7
Alireza Sadeghi <alirezagt4x@gmail.com>
2026-06-19 21:43:59 +0330

    Fix console bugs of AddTodo component

--- src/Main/AddTodo.jsx
+++ src/Main/AddTodo.jsx
@@ -19,20 +19,20 @@ export default function AddTodo() {
 					fill="none"
 					xmlns="http://www.w3.org/2000/svg"
 				>
-					<g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+					<g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
 					<g
 						id="SVGRepo_tracerCarrier"
-						stroke-linecap="round"
-						stroke-linejoin="round"
+						strokeLinecap="round"
+						strokeLinejoin="round"
 					></g>
 					<g id="SVGRepo_iconCarrier">
 						{" "}
 						<path
 							d="M6 12H18M12 6V18"
 							stroke="#ffffff"
-							stroke-width="2"
-							stroke-linecap="round"
-							stroke-linejoin="round"
+							strokeWidth="2"
+							strokeLinecap="round"
+							strokeLinejoin="round"
 						></path>{" "}
 					</g>
 				</svg>
