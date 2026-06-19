27b2ae18bbba1bd7aa09ae97b29c3f303f72b1eb
Alireza Sadeghi <alirezagt4x@gmail.com>
2026-06-19 20:23:32 +0330

    Fix Empty component bugs

--- src/Main/Empty.jsx
+++ src/Main/Empty.jsx
@@ -8,8 +8,10 @@ export default function Empty() {
 				src="/public/Animations/NothingHere.json"
 				className="md:w-100 w-80"
 			></lottie-player>
-            <p>No task in this category.</p>
-            <p>Use + to add one</p>
+			<div className="flex flex-col items-center justify-center text-black dark:text-white">
+				<p>No tasks in this category.</p>
+				<p>Use + to add one</p>
+			</div>
 		</div>
 	);
 }
