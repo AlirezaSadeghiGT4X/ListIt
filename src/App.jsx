67edad1aeb78cd53d8236fa9ad36a9f51a8f507a
Alireza Sadeghi <alirezagt4x@gmail.com>
2026-06-24 00:44:46 +0330

    Add notfound animation and router in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import Header from "./Header/Header";
 import Main from "./Main/Main";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NotFound from "./NotFound/NotFound";
 export default function App() {
 	return (
-		<div className="flex flex-col h-screen xl:overflow-hidden">
-			<Header />
-			<Main />
-		</div>
+		<Router>
+			<Routes>
+				<Route
+					path="/"
+					element={
+						<div className="flex flex-col h-screen xl:overflow-hidden">
+							<Header />
+							<Main />
+						</div>
+					}
+				/>
+				<Route path="*" element={<NotFound/>}/>
+			</Routes>
+		</Router>
 	);
 }
