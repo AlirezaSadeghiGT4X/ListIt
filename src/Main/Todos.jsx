1124219fa08578281fab2f9ae588eb24c6e1453c
Alireza Sadeghi <alirezagt4x@gmail.com>
2026-06-24 23:55:48 +0330

    Fix change between categories bug

--- src/Main/Todos.jsx
+++ src/Main/Todos.jsx
@@ -1,14 +1,40 @@
+/* eslint-disable react-hooks/set-state-in-effect */
 import { useLocalStorage } from "usehooks-ts";
 import AddTodo from "./AddTodo";
 import Empty from "./Empty";
 import TodoList from "./TodoList";
+import { useEffect, useState } from "react";
 
 export default function Todos({ selectedCategory, setSelectedCategory }) {
 	const [data, setValue, removeValue] = useLocalStorage("todos", []);
 	let todos = data;
+	let [status, setStatus] = useState(null);
+	let [categoryStatus, setCategoryStatus] = useState(null);
+	useEffect(() => {
+		setCategoryStatus(null);
+		for (const todo of todos) {
+			if (todo.category == selectedCategory) {
+				setCategoryStatus(true);
+				break;
+			}
+		}
+		if (todos.length > 0 && categoryStatus != null) {
+			setStatus(true);
+		} else if (
+			selectedCategory == undefined &&
+			todos != undefined &&
+			status != true
+		) {
+			setStatus(true);
+		} else if (selectedCategory == "All") {
+			setStatus(true);
+		} else {
+			setStatus(null);
+		}
+	}, [categoryStatus, data, selectedCategory, status, todos]);
 	return (
 		<div className="relative w-full">
-			{todos == null || todos == undefined || todos == "" ? (
+			{status != true ? (
 				<div className="mb-36">
 					<Empty />
 				</div>
